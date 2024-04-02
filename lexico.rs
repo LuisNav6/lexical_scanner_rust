5dd777866b712b7c5415c39d0086570ccc1e8d56
LuisNav6 <113213452+LuisNav6@users.noreply.github.com>
2024-04-02 13:22:56 -0600

    fix bug un NumReal and feature in add counters and reserved words "cin" and "cout"

--- lexico.rs
+++ lexico.rs
@@ -23,6 +23,8 @@ enum TokenType {
     AND,
     OR,
     RETURN,
+    CIN,
+    COUT,
 
     // Tokens de múltiples caracteres
     ID,
@@ -53,6 +55,12 @@ enum TokenType {
     COMMA,     // coma
     SEMICOLON, // punto y coma
     ASSIGN,    // asignación
+    
+    //Incrementador
+    INCREMENT,
+    
+    //Decrementador
+    DECREMENT,
 
     // Símbolo de comentario múltiple no cerrado
     InMultipleComment,
@@ -110,6 +118,8 @@ fn reserved_lookup(s: &str) -> TokenType {
         "main" => TokenType::MAIN,
         "return" => TokenType::RETURN,
         "/*" => TokenType::InMultipleComment,
+        "cin" => TokenType::CIN,
+        "cout" => TokenType::COUT,
         _ => TokenType::ID,
     }
 }
@@ -210,8 +220,26 @@ fn get_token(content: &str) -> (Vec<(TokenType, String, usize, usize)>, Vec<(Tok
 
                             }
                         }
-                        '+' => tokens.push((TokenType::PLUS, "+".to_string(), lineno, column_number)),
-                        '-' => tokens.push((TokenType::MINUS, "-".to_string(), lineno, column_number)),
+                        '+' => {
+                            let next_char = get_next_char(content, &mut linepos, bufsize);
+                            if next_char == '+' {
+                                tokens.push((TokenType::INCREMENT, "++".to_string(), lineno, column_number - 1));
+                            } else {
+                                tokens.push((TokenType::PLUS, "+".to_string(), lineno, column_number - 1));
+                                unget_next_char(&mut linepos);
+
+                            }
+                        }
+                        '-' => {
+                            let next_char = get_next_char(content, &mut linepos, bufsize);
+                            if next_char == '-' {
+                                tokens.push((TokenType::DECREMENT, "--".to_string(), lineno, column_number - 1));
+                            } else {
+                                tokens.push((TokenType::MINUS, "-".to_string(), lineno, column_number - 1));
+                                unget_next_char(&mut linepos);
+
+                            }
+                        }
                         '*' => tokens.push((TokenType::TIMES, "*".to_string(), lineno, column_number)),
                         '%' => tokens.push((TokenType::MODULO, "%".to_string(), lineno, column_number)),
                         '^' => tokens.push((TokenType::POWER, "^".to_string(), lineno, column_number)),
@@ -241,29 +269,34 @@ fn get_token(content: &str) -> (Vec<(TokenType, String, usize, usize)>, Vec<(Tok
                     unget_next_char(&mut linepos); // Retornar un carácter
                 }
             }
-            StateType::InNum => {
-                if c.is_digit(10) {
-                    token_string.push(c);
-                } else if c == '.' {
-                    state = StateType::InReal;
-                    token_string.push(c);
-                } else {
-                    tokens.push((TokenType::NumInt, token_string.clone(), lineno, (column_number - 1)));
-                    token_string.clear();
-                    state = StateType::Start;
-                    unget_next_char(&mut linepos); // Retornar un caráct
-                }
-            }
-            StateType::InReal => {
-                if c.is_digit(10) {
-                    token_string.push(c);
-                } else {
-                    tokens.push((TokenType::NumReal, token_string.clone(), lineno, (column_number - 1)));
-                    token_string.clear();
-                    state = StateType::Start;
-                    unget_next_char(&mut linepos); // Retornar un caráct
-                }
-            }
+             StateType::InNum => {
+        if c.is_digit(10) {
+            token_string.push(c);
+        } else if c == '.' {
+            state = StateType::InReal;
+            token_string.push(c);
+        } else {
+            tokens.push((TokenType::NumInt, token_string.clone(), lineno, (column_number - 1)));
+            token_string.clear();
+            state = StateType::Start;
+            unget_next_char(&mut linepos); // Retornar un carácter
+        }
+    }
+    StateType::InReal => {
+        if c.is_digit(10) {
+            token_string.push(c);
+        } else if token_string.ends_with('.') {
+            errors.push((TokenType::ERROR, token_string.clone(), lineno, (column_number - 1)));
+            token_string.clear();
+            state = StateType::Start;
+            unget_next_char(&mut linepos);
+        } else {
+            tokens.push((TokenType::NumReal, token_string.clone(), lineno, (column_number - 1)));
+            token_string.clear();
+            state = StateType::Start;
+            unget_next_char(&mut linepos); // Retornar un caráct
+        }
+    }
             StateType::InComment => {
                 if c == '\n' || c == '\0' {
                     state = StateType::Start;
@@ -298,69 +331,55 @@ fn get_token(content: &str) -> (Vec<(TokenType, String, usize, usize)>, Vec<(Tok
 
 
 fn main() {
-    let file_content = r#"
-    //numeros enteros y reales con o sin signo
-    123
--456
-0
-12.34
--56.78
-0.123
--0.456
-789.0
-//identificadores
-variable
-_underscore
-var123
-variable_123
-Var123_
-/* 
-comentario
-de
-multiples
-lineas */
-//palabras reservadas
-if
-else
-do
-while
-switch
-case
-end
-repeat
-until
-read
-write
-int
-double
-main
-return
-//operadores aritméticos
-+
--
-*
-/
-%
-^
-//operadores relacionales
-==
-!=
-<
-<=
->
->=
-//operadores lógicos
-&
-|
-//símbolos
-(
-)
+    let file_content = r#" main sum@r 3.14+main)if{32.algo
+34.34.34.34
 {
-}
-,
-;
-//asignacion
+int x,y,z;
+real a,b,c;
+ suma=45;
+x=32.32;
+x=23;
+y=2+3-1;
+z=y+7;
+y=y+1;
+a=24.0+4-1/3*2+34-1;
+x=(5-3)*(8/2);
+y=5+3-2*4/7-9;
+z=8/2+15*4;
+y=14.54;
+if(2>3)then
+        y=a+3;
+  else
+      if(4>2 && )then
+             b=3.2;
+       else
+           b=5.0;
+       end;
+       y=y+1;
+end;
+a++;
+c--;
+x=3+4;
+do
+   y=(y+1)*2+1;
+   while(x>7){x=6+8/9*8/3;   
+    cin x; 
+   mas=36/7; 
+   };
+
+ until(y=
+
+
 =
+
+
+
+5);
+ while(y==0){
+    cin mas;
+    cout x;
+};
+}
     "#;
     
     let (tokens, errors) = get_token(&file_content);
